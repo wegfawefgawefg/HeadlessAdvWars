244d6227b4c23f0a2b6b42781c0915ffe1cc110a
wegfawefgawefg <668es218pur@gmail.com>
2021-08-30 00:35:12 -0700

    todo

---
+++ todo.py
@@ -0,0 +1,10 @@
+#- damage calculator
+- path calculator (unique cases player manual override)
+- action enumerator
+- make some tiles
+- make rest of unit classes 
+- production facilities
+- income calculator
+- player game states
+- turn end check
+- unit states during match
